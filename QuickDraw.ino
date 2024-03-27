46de5b4e30bd8085ceb7919998739c2746318312
SkinnyJosiah <94023705+SkinnyJosiah@users.noreply.github.com>
2024-03-27 09:48:19 -0600

    Create QuickDraw.ino

---
+++ QuickDraw.ino
@@ -0,0 +1,25 @@
+// Adafruit GFX Library - Version: Latest 
+#include <Adafruit_GFX.h>
+#include <Adafruit_GrayOLED.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <gfxfont.h>
+
+// Adafruit ST7735 and ST7789 Library - Version: Latest 
+#include <Adafruit_ST7735.h>
+#include <Adafruit_ST7789.h>
+#include <Adafruit_ST77xx.h>
+
+// Both of these are libraries required for setting up the Adafruit LCD.
+
+/*
+
+*/
+
+void setup() {
+    
+}
+
+void loop() {
+    
+}
