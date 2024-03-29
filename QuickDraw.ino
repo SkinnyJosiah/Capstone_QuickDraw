09c46cd6fd2e95580a284fe80df1012e898ef9f9
SkinnyJosiah <94023705+SkinnyJosiah@users.noreply.github.com>
2024-03-29 11:51:37 -0600

    Update QuickDraw.ino

--- QuickDraw.ino
+++ QuickDraw.ino
@@ -1,3 +1,18 @@
+// Welcome to Quick Draw.
+
+// SdFat - Adafruit Fork - Version: Latest 
+#include <BufferedPrint.h>
+#include <FreeStack.h>
+#include <MinimumSerial.h>
+#include <RingBuf.h>
+#include <SdFat.h>
+#include <SdFatConfig.h>
+#include <sdios.h>
+
+// Adafruit ImageReader Library - Version: Latest 
+#include <Adafruit_ImageReader.h>
+#include <Adafruit_ImageReader_EPD.h>
+
 // Adafruit GFX Library - Version: Latest 
 #include <Adafruit_GFX.h>
 #include <Adafruit_GrayOLED.h>
@@ -10,11 +25,16 @@
 #include <Adafruit_ST7789.h>
 #include <Adafruit_ST77xx.h>
 
-// Both of these are libraries required for setting up the Adafruit LCD.
+// These are libraries required for setting up the Adafruit LCD.  
 
-/*
+// The first library is SDFat, which provides access to SD memory cards.
 
-*/
+// The second is Adafruit's ImageReader library. Self explanatory.
+
+// The third is Adafruit's GFX Library. "The core class that all our other graphics libraries derive from."
+
+// Fourth is Adafruit's ST7735 (our model) library. Basically, makes things work.
+
 
 void setup() {
     
